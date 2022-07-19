1b5a0e0c14a41e1549b3f27c8ebe970d529859c0
luiz-cazarin <57879201+luiz-cazarin@users.noreply.github.com>
2022-07-19 16:03:52 -0300

    fix: base project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="bg-red-100">Base do projeto</div>
+</template>
+
+<script>
+import './assets/tailwind.css';
+
+export default {
+  name: 'App',
+  components: {
+  },
+};
+</script>
+
+<style>
+</style>
